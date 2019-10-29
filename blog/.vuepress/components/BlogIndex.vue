ff580e08769b7ab37e82971d4f8dedc255e87caa
Josh Peak <neozenith.dev@gmail.com>
2019-10-30 10:29:39 +1100

    Update BlogIndex component

--- blog/.vuepress/components/BlogIndex.vue
+++ blog/.vuepress/components/BlogIndex.vue
@@ -1,16 +1,47 @@
 <template>
 <div>
     <div v-for="post in posts">
-        <h2>
-            <router-link :to="post.path">{{ post.frontmatter.title }}</router-link>
-        </h2>
-        
-        <p>{{ post.frontmatter.description }}</p>
+        <div v-if="post.frontmatter.image" align=center class="post-pic">
+            <img 
+                class="post-pic-img"
+                :src="post.frontmatter.image" />
+        </div>
+        <div class="post-detail">
+            <h2>
+                <router-link :to="post.path">{{ post.frontmatter.title }}</router-link>
+            </h2>
+            
+            <p>{{ post.frontmatter.description }}</p>
 
-        <p><router-link :to="post.path">Read more</router-link></p>
+            <p align=right><router-link :to="post.path">Read more</router-link></p>
+        </div>
     </div>
 </div>
 </template>
+
+<style>
+  .post-detail {
+  }
+  .post-pic {
+    float: left;
+    display: block;
+    padding: auto;
+    width: 8.6rem;
+    height: 8.6rem; 
+    margin-right: 1em;
+    overflow: hidden;
+  }
+  .post-pic-img:hover {
+    opacity: 0.5;
+  }
+  .post-pic-img {
+    display: block;
+    opacity: 0.8; 
+    height: 8.6rem;
+    position: absolute;
+    clip: rect(0px, 8.6rem, 8.6rem, 0px);
+  }
+</style>
 
 <script>
 export default {
